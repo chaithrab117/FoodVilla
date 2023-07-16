7097108aa9ca43dae1614ceae53e2afbeaed64e9
chaithrab117 <chaithrab.117@gmail.com>
2023-07-16 20:57:57 +0530

    first commit

---
+++ src/component/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../style/Footer.css'
+export const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <h1>This site is developed by Chaithra B</h1>
+    </div>
+  )
+}
